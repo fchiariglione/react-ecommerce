e1315b18b731fb66dfa2b832c9de8bbf3218bd8c
Francisco Chairiglione <fchiariglione@gmail.com>
2023-10-10 21:23:01 -0300

    new components and packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import { Container } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom"
 
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
-        <h1>Welcome to Proshop</h1>
+        <Container>
+          <Outlet />
+        </Container>
 
       </main>
       <Footer />
